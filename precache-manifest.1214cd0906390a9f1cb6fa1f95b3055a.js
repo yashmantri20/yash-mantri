03174161ce86fab683cc62a471a42d8e61346a75
yash mantri <mantriyash2025@gmail.com>
2020-08-24 19:18:50 +0530

    Updates

---
+++ precache-manifest.1214cd0906390a9f1cb6fa1f95b3055a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd98b9f599e2ef52ce410a888c04f948",
+    "url": "/yash-mantri/index.html"
+  },
+  {
+    "revision": "7f5f51edd822a2f9686d",
+    "url": "/yash-mantri/static/css/3.4ea14309.chunk.css"
+  },
+  {
+    "revision": "7d8f6f15f4516809f749",
+    "url": "/yash-mantri/static/css/4.6e6255c1.chunk.css"
+  },
+  {
+    "revision": "bef39496896911e0618b",
+    "url": "/yash-mantri/static/css/main.5e9d709a.chunk.css"
+  },
+  {
+    "revision": "c9c6f0a23aaf9f9278fd",
+    "url": "/yash-mantri/static/js/2.76af480f.chunk.js"
+  },
+  {
+    "revision": "7f5f51edd822a2f9686d",
+    "url": "/yash-mantri/static/js/3.91ddf0a3.chunk.js"
+  },
+  {
+    "revision": "7d8f6f15f4516809f749",
+    "url": "/yash-mantri/static/js/4.b54308a2.chunk.js"
+  },
+  {
+    "revision": "bef39496896911e0618b",
+    "url": "/yash-mantri/static/js/main.76e4009d.chunk.js"
+  },
+  {
+    "revision": "5f376d0394decc8affd5",
+    "url": "/yash-mantri/static/js/runtime-main.f00c3fe2.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/yash-mantri/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/yash-mantri/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/yash-mantri/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/yash-mantri/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/yash-mantri/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "bdcaae4177a9b43505d8b437bde11259",
+    "url": "/yash-mantri/static/media/skillNew.bdcaae41.svg"
+  }
+]);
